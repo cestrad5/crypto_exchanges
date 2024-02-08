5ba340db9b53be5fdcfbd526e2652c324f4b67ae
CAMILO <cestrad5@gmail.com>
2024-02-08 05:07:05 -0500

    First commit

---
+++ src/data/monedas.js
@@ -0,0 +1,18 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'COP', nombre: 'Peso Colombiano'},
+    { id: 'ARS', nombre: 'Peso Argentino'},
+    { id: 'BRL', nombre: 'Real Brasileño'},
+    { id: 'JPY', nombre: 'Yen Japonés'},
+    { id: 'ZAR', nombre: 'Rand Sudafricano'},
+    { id: 'RUB', nombre: 'Rublo Ruso'},
+    { id: 'TRY', nombre: 'Lira Turca'},
+    { id: 'INR', nombre: 'Rupia India'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Libra Esterlina'},
+]
+
+export {
+    monedas
+}
